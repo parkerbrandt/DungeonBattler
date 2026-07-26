e6dc3160a9b24a73960aafdedd456bccdc473fc9
Parker Brandt <parker.brandt101@gmail.com>
2026-07-26 18:18:09 -0500

    Added screens and started implementing Start Screen

--- core/src/com/lucentus/dungeon/DungeonBattler.java
+++ core/src/com/lucentus/dungeon/DungeonBattler.java
@@ -1,31 +1,55 @@
 package com.lucentus.dungeon;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.lucentus.dungeon.screens.StartScreen;
 
-public class DungeonBattler extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class DungeonBattler extends Game {
+
+	/*
+	 * Static Class Members
+	 */
+	public static final int VIEWPORT_HEIGHT = 1080;
+	public static final int VIEWPORT_WIDTH = 1920;
+
+
+	/*
+	 * Properties
+	 */
+	public ShapeRenderer shape;
+	public SpriteBatch batch;
+	public BitmapFont font;
+
+
+	/*
+	 * Overridden Methods
+	 */
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 
+		shape = new ShapeRenderer();
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+
+		// Set to title screen
+		this.setScreen(new StartScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
+		shape.dispose();
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
 }
--- core/src/com/lucentus/dungeon/screens/StartScreen.java
+++ core/src/com/lucentus/dungeon/screens/StartScreen.java
@@ -1,4 +1,92 @@
 package com.lucentus.dungeon.screens;
 
-public class StartScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.lucentus.dungeon.DungeonBattler;
+
+
+/**
+ * Title/Start Screen
+ */
+public class StartScreen implements Screen {
+
+    /*
+     * Properties
+     */
+    private final DungeonBattler game;
+
+    // View
+    private OrthographicCamera camera;
+
+    // UI
+    private final Skin skin;
+    private final Stage stage;
+
+
+    /*
+     * Constructors
+     */
+    public StartScreen(DungeonBattler game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, DungeonBattler.VIEWPORT_WIDTH, DungeonBattler.VIEWPORT_HEIGHT);
+
+        // Initialize skin for UI
+        skin = new Skin(Gdx.files.internal("assets/resources/ui/skins/uiskin.json"));
+
+        // Initialize Scene2d UI components
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+    }
+
+
+    /*
+     * Overridden Methods
+     */
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        camera.update();
+        game.batch.begin();
+        game.batch.end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
